94885f130c4c2c68f7469c33b116b61e453cdeb9
Yevhen <example@gmail.com>
2023-06-08 15:52:43 +0200

    Упрощение с помощью цыклов

--- js/script.js
+++ js/script.js
@@ -10,13 +10,27 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-      b = prompt("На сколько оцените его", ""),
-      c = prompt("Один из последних просмотренных фильмов", ""),
-      d = prompt("На сколько оцените его", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+          b = prompt("На сколько оцените его", "");
 
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log ("Watching low films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+    console.log("Classic watching");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You awesome watching");
+} else {
+    console.log("error");
+}
 
 console.log(personalMovieDB);
